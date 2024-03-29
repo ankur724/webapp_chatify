cfa22faeaed1b730f1f95a8ece35239c1df1fac2
Ankur <ankursaxena2002@gmail.com>
2024-03-29 18:45:49 +0530

    ac

--- server/index.js
+++ server/index.js
@@ -18,12 +18,7 @@ app.get("/", (req, res) => {
 });
 
 // Serve static files in production
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "chatify", "build")));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, "chatify", "build", "index.html"));
-    }); 
-}
+
 
 const server = http.createServer(app);
 const io = socketIO(server);
